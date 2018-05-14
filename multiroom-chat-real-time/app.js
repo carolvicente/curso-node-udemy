3028a608f8ac6961c7f2f5347f050d4483fea09a
Caroline-Vicente <Caroline.Vicente@ibm.com>
2018-05-14 14:49:04 -0300

    new modules

--- multiroom-chat-real-time/app.js
+++ multiroom-chat-real-time/app.js
@@ -2,6 +2,19 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(80, function(){
+var server = app.listen(80, function(){
     console.log('Servidor online');
+});
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* criar a conexão por websocket */
+io.on('connection', (socket) =>{
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectou');
+    });
 });
